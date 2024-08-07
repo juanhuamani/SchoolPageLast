60dbfebb12138f50018eb0302413bb095c37af93
JuanDiego <jhuamaniv@unsa.edu.pe>
2024-08-07 10:00:17 -0500

    refactor: Update table.blade.php to use DateHelper for formatting dates and improve table structure

--- resources/views/livewire/courses/table.blade.php
+++ resources/views/livewire/courses/table.blade.php
@@ -1,3 +1,7 @@
+<?php
+    $helper = new \App\Helpers\DateHelper();
+?>
+
 <div class="py-12">
     <div class="mx-auto max-w-screen-xl px-4 text-center">
         <div class="flex justify-between gap-3">
@@ -21,8 +25,31 @@
             <x-primary-button wire:click="filter">{{ __('Search') }}</x-primary-button>
         </div>
         <table class="w-full mt-10">
-            <x-courses.table-header :week="$week" :weeknumber="$weekNumber" />
-            <x-courses.table-body :users="$users" :attendance="$attendance" :week="$week" />
+            <thead>
+                <tr class="text-center font-semibold text-gray-900 bg-gray-100 uppercase border-b">
+                    <th class="px-4 py-3"></th>
+                    @foreach ($week as $day)
+                        <th class="px-4 py-3">{!! $helper->formatDayHeader($day) !!}<br>{{ $day }}</th>
+                    @endforeach
+                </tr>
+            </thead>
+
+            <tbody class="bg-white">
+                @foreach ($users as $user)
+                    <tr class="text-gray-700">
+                        <td class="px-4 py-3 border">{{ $user->name }}</td>
+                        @foreach ($week as $day)
+                            <td>
+                                @if (isset($attendance[$user->id][$day]) && $attendance[$user->id][$day])
+                                    <x-present />
+                                @else
+                                    <x-absent />
+                                @endif
+                            </td>
+                        @endforeach
+                    </tr>
+                @endforeach
+            </tbody>
         </table>
     </div>
 </div>
